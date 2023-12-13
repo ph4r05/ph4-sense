7535cbd6185e4bac03d5be4ca93811b83df56d6d
Dusan Klinec <dusan.klinec@gmail.com>
2023-12-13 09:02:49 +0100

    wip

---
+++ ph4_sense_base/mods_mp/uart.py
@@ -0,0 +1,18 @@
+import machine
+
+from ph4_sense_base.mods.uart import ModUart
+from ph4_sense_base.support.uart_mp import UartMp
+
+
+class ModUartMp(ModUart):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def get_uart_builder(self, desc):
+        if desc["type"] != "uart":
+            raise ValueError("Only uart type is supported")
+
+        def builder(**kwargs):
+            return UartMp(machine.UART(desc["port"], **kwargs))
+
+        return builder
