34f5227b0b279e16ecd6e43e4dce8d1241273e11
Dusan Klinec <dusan.klinec@gmail.com>
2023-12-24 15:01:02 +0100

    wip

---
+++ ph4_sense_base/mods/__init__.py
@@ -0,0 +1,6 @@
+class BaseMod:
+    def __init__(self):
+        pass
+
+    def load_config(self, js):
+        pass
--- ph4_sense_base/mods/uart.py
+++ ph4_sense_base/mods/uart.py
@@ -1,6 +1,9 @@
-class ModUart:
+from ph4_sense_base.mods import BaseMod
+
+
+class UartMod(BaseMod):
     def __init__(self, *args, **kwargs):
-        pass
+        super().__init__()
 
     def get_uart_builder(self, desc):
         return None
