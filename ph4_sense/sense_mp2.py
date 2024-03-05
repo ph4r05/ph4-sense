ed0e824d273dbb0f28a032101f7163495185e5d7
Dusan Klinec <dusan.klinec@gmail.com>
2024-03-05 23:22:19 +0100

    wip

---
+++ ph4_sense/sense_mp2.py
@@ -0,0 +1,68 @@
+import machine
+
+from ph4_sense_base.adapters import updateLogger
+from ph4_sense_base.logger_mp import MpLogger
+from ph4_sense_base.sensei_base import SenseiBase
+from ph4_sense_base.support.uart_mp import UartMp
+
+# import network
+# from umqtt.robust import MQTTClient
+
+
+# from ph4_sense_base.utils import try_fnc
+
+# Set up your SPG30 sensor pin connections
+# https://randomnerdtutorials.com/esp32-i2c-communication-arduino-ide/
+# SPG30_SCL_PIN = 22
+# SPG30_SDA_PIN = 21
+
+
+# TODO: IMPLEMENT with mods
+# TODO: mod for sensors, sensor set?
+class SenseiMp(SenseiBase):
+    def __init__(
+        self,
+        is_esp32=True,
+        has_wifi=True,
+        has_aht=True,
+        has_sgp30=True,
+        has_ccs811=True,
+        has_scd4x=True,
+        has_sps30=False,
+        has_hdc1080=False,
+        has_zh03b=False,
+        has_sgp41=False,
+        scl_pin=22,
+        sda_pin=21,
+    ):
+        super().__init__(
+            is_esp32=is_esp32,
+            has_wifi=has_wifi,
+            scl_pin=scl_pin,
+            sda_pin=sda_pin,
+        )
+
+        updateLogger(MpLogger(self.log_fnc))
+
+    def get_uart_builder(self, desc):
+        if desc["type"] != "uart":
+            raise ValueError("Only uart type is supported")
+
+        def builder(**kwargs):
+            return UartMp(machine.UART(desc["port"], **kwargs))
+
+        return builder
+
+    def publish_msg(self, topic: str, message: str):
+        self.mqtt_client.publish(topic, message)
+        self.print(f"Published {topic}:", message)
+
+
+def main(**kwargs):
+    sensei = SenseiMp(**kwargs)
+    sensei.main()
+
+
+# Run the main program
+if __name__ == "__main__":
+    main()
--- ph4_sense/sensors/ccs811base.py
+++ ph4_sense/sensors/ccs811base.py
@@ -1,5 +1,4 @@
-from ph4_sense.adapters import const
-from ph4_sense.sensors.common import ccs811_err_to_str, ccs811_status_to_str
+from ph4_sense_base.adapters import const
 
 try:
     from typing import Optional, Tuple
@@ -9,6 +8,41 @@ except ImportError:
 
 
 DRIVE_MODE_1SEC = const(0x1)
+
+
+def ccs811_err_to_str(err: int) -> str:
+    ret = ""
+    err = err or 0
+    if err & 0x1:
+        ret += "Wi "  # WRITE_REG_INVALID
+    if err & 0x2:
+        ret += "Ri "  # READ_REG_INVALID
+    if err & 0x4:
+        ret += "Mi "  # MEASMODE_INVALID
+    if err & 0x8:
+        ret += "Mr "  # MAX_RESISTANCE
+    if err & 0x10:
+        ret += "Hf "  # HEATER_FAULT
+    if err & 0x20:
+        ret += "Hs "  # HEATER_SUPPLY
+    return ret
+
+
+def ccs811_status_to_str(status: int) -> str:
+    ret = ""
+    if status & 0x1:
+        ret += "Er "  # Error
+    if status & 0x8:
+        ret += "Dr "  # Data ready
+    if status & 0x10:
+        ret += "F+ "  # Valid Fw loaded
+    else:
+        ret += "F- "  # Valid Fw loaded
+    if status & 0x80:
+        ret += "R+ "  # FW_MODE, 1 = ready to measure
+    else:
+        ret += "R- "  # FW_MODE, 1 = ready to measure
+    return ret
 
 
 class ICSS811:
