e790cfac1ade9f77eca46920e6a3c3bffce9fcf7
Dusan Klinec <dusan.klinec@gmail.com>
2024-06-21 23:05:39 +0200

    shelly - add curtain control with state automaton

---
+++ shelly/shelly-curt-auto.js
@@ -0,0 +1,128 @@
+// State definitions
+const State = {
+  IDLE: "IDLE",
+  STOPPING: "STOPPING",
+  STOPPING_TILT: "STOPPING_TILT",
+  MOVING_TO_POSITION: "MOVING_TO_POSITION",
+  TILTING: "TILTING",
+  TILTING2: "TILTING2"
+};
+
+let currentState = State.IDLE;
+let currentOperation = null;
+let currentCoverStatus = null;
+
+function transitionState(newState, operation) {
+  currentState = newState;
+  currentOperation = operation;
+  print("State transition to ", currentState, "with operation ", operation);
+}
+
+function performTiltClosing(){
+  Shelly.call("Cover.Close", {
+      id: 0,
+      duration: 1.2,
+    }, function (r, error_code, error_message, userdata1) {
+      if (r !== null){
+        transitionState(State.IDLE, {error: error_code})
+      }
+    });
+}
+
+function performTilt(duration) {
+  Shelly.call("Cover.Open", {
+    id: 0,
+    duration: duration,
+  }, function (r, error_code, error_message, userdata1) {
+      if (r !== null){
+        transitionState(State.IDLE, {error: error_code})
+      }
+    });
+}
+
+function loadStatus(callback) {
+  Shelly.call("Cover.GetStatus", { id: 0 }, function (r, error_code, error_message, userdata1) {
+    print("posAndTilt status: ", JSON.stringify(r));
+    currentCoverStatus = r
+    callback()
+  });
+}
+
+function processEvent() {
+    switch (currentState) {
+      case State.STOPPING:
+        if (currentOperation) {
+          const pos = currentOperation.pos;
+          if (currentCoverStatus && currentCoverStatus.current_pos === pos) {
+            transitionState(State.MOVING_TO_POSITION, currentOperation);
+            processEvent();
+          } else {
+            transitionState(State.MOVING_TO_POSITION, currentOperation);
+            Shelly.call("Cover.GoToPosition", { id: 0, pos: pos });
+          }
+        } else {
+          transitionState(State.IDLE, null);
+        }
+        break;
+
+      case State.MOVING_TO_POSITION:
+        if (currentOperation.tilt_duration < 0.05) {
+          transitionState(State.TILTING2, currentOperation);
+          return processEvent();
+        }
+        if (currentCoverStatus && currentCoverStatus.current_pos - currentOperation.pos >= 2) {
+          transitionState(State.TILTING, currentOperation);
+          return processEvent();
+        } else {
+          transitionState(State.TILTING, currentOperation);
+          performTiltClosing();
+        }
+        break;
+
+      case State.TILTING:
+        if (currentOperation.tilt_duration < 0.05) {
+          transitionState(State.TILTING2, currentOperation);
+          return processEvent();
+        }
+        transitionState(State.TILTING2, currentOperation);
+        performTilt(currentOperation.tilt_duration);
+        break;
+
+      case State.TILTING2:
+        transitionState(State.IDLE, null);
+        print("Tilting done.");
+        break;
+    }
+}
+
+// Set up a global event handler for all state transitions
+Shelly.addEventHandler(function (event) {
+  print("Global event handler: ", JSON.stringify(event));
+
+  if (event.info && (event.info.event === "stopped" || event.info.event === "closed")) {
+    processEvent();
+  }
+}, null);
+
+function entryPoint(stop_state, transition_state, data) {
+    loadStatus(function() {
+      if (currentState !== State.IDLE) {
+        transitionState(stop_state, data);
+        Shelly.call("Cover.Stop", { id: 0 }, function (r, error_code, error_message, userdata1) {
+            print("Stop callback", r, error_code, error_message)
+            processEvent();
+        });
+      } else {
+        transitionState(transition_state, data);
+        processEvent();
+      }
+    })
+}
+
+function tilt(tilt_duration) {
+  entryPoint(State.STOPPING_TILT, State.MOVING_TO_POSITION, {tilt_duration: tilt_duration})
+}
+
+function posAndTilt(pos, tilt_duration) {
+  entryPoint(State.STOPPING, State.STOPPING, { pos:pos, tilt_duration:tilt_duration })
+}
--- shelly/shelly-curt.js
+++ shelly/shelly-curt.js
@@ -28,9 +28,78 @@ function tilt(tilt_duration) {
       id: 0,
       duration: 1.2,
     }, function (r, error_code, error_message, userdata1) {
-      inProgress = true;
+      inProgress = r === null;
     });
 }
 
-// tilt(duration);
-// TODO: go to pos and tilt
+function posAndTilt(pos, duration) {
+  print("posAndTilt called")
+  if (inProgress){
+    return
+  }
+
+  let stopped = false
+  let subscription = -1;
+  let subscriptionEvent= -1;
+
+  Shelly.call("Cover.Stop", { id: 0 }, function (r, error_code, error_message, userdata1) {
+    Shelly.call("Cover.GetStatus", { id: 0 }, function (r, error_code, error_message, userdata1) {
+      print("posAndTilt status: ", JSON.stringify(r));
+      if (r == null) {
+        return
+      }
+      if (r.current_pos === pos) {
+        print("posAndTilt already at pos, just tilt")
+        tilt(duration)
+        return
+      }
+
+      let unreg = function () {
+        print("unreg now")
+        if (subscription >= 0) {
+          Shelly.removeStatusHandler(subscription);
+          subscription = -1;
+        }
+        if (subscriptionEvent >= 0) {
+          Shelly.removeEventHandler(subscriptionEvent);
+          subscriptionEvent = -1;
+        }
+      }
+
+      let handler = function (event) {
+        // print("posAndTilt status handler: ", JSON.stringify(event));
+        if (event && event.delta && (event.delta.state === "stopped" || event.delta.state === "closed" || event.delta.state === "open")) {
+          print("posAndTilt status, Go to pos done, now tilt")
+          unreg()
+          tilt(duration);
+        }
+      };
+      let handlerEvent = function (event) {
+        // print("posAndTilt event handler: ", JSON.stringify(event));
+        if (event && event.info && (event.info.event === "stopped" || event.info.event === "closed" || event.info.event === "open")) {
+          print("posAndTilt evt, Go to pos done, now tilt")
+          unreg()
+          tilt(duration)
+        }
+      };
+
+      subscription = Shelly.addStatusHandler(handler, null);
+      subscriptionEvent = Shelly.addEventHandler(handlerEvent, null);
+      Timer.set(60000, false, function(){
+          print("Failsafe unreg")
+          unreg()
+      });
+      Shelly.call("Cover.GoToPosition", {
+        id: 0,
+        pos: pos,
+      }, function (r, error_code, error_message, userdata1) {
+        print("posAndTilt Cover.GoToPosition done", r)
+        if (r !== null) {
+          unreg()
+        }
+      })
+    })
+  })
+}
+
+// posAndTilt(26, 1)
