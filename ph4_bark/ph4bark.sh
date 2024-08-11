a7a2ffed1a5a21fb7243ec4d295c2e6d33315839
Dusan Klinec <dusan.klinec@gmail.com>
2024-08-11 20:41:32 +0200

    fix ph4bark, add logrotate

--- ph4_bark/ph4bark.sh
+++ ph4_bark/ph4bark.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env sh
+export LIBROSA_CACHE_DIR=/tmp/librosa_cache
+export NUMBA_CACHE_DIR=/tmp
+# export LIBROSA_CACHE_LEVEL=0
+# mkdir -p LIBROSA_CACHE_DIR
 exec python3.12 /usr/local/bin/ph4-bark $*
