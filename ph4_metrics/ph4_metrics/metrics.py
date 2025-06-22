6bf05b723fe4743562ee688ca0ffd0332523efb0
Dusan Klinec <dusan.klinec@gmail.com>
2025-06-22 20:05:34 +0200

    metrics: auto discovery

--- ph4_metrics/ph4_metrics/metrics.py
+++ ph4_metrics/ph4_metrics/metrics.py
@@ -24,34 +24,79 @@ def get_hostname():
 
 
 class MetricsCollector:
+    BIRTH_TOPIC = "homeassistant/status"
+
     def __init__(self):
         self.mqtt_client = None
         self.mqtt_broker = os.getenv("MQTT_BROKER", "localhost")
         self.mqtt_port = 1883
         self.mqtt_topic_sub = self.build_mqtt_topic()
         self.err_ssd_shown = False
+        self.discovery_sent = False
         self.metrics = Metrics()
 
     @classmethod
     def build_mqtt_topic(cls) -> str:
         return os.getenv("MQTT_TOPIC", f"metrics/{get_hostname()}")
 
+    @classmethod
+    def topic_normalize(cls, topic: str) -> str:
+        return topic.replace("/", "_").replace("-", "_").replace(".", "_")
+
     def create_mqtt_client(self):
         client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, self.mqtt_topic_sub)
         client.on_message = self.mqtt_callback
         client.connect(self.mqtt_broker, self.mqtt_port, keepalive=60)
         client.subscribe(self.mqtt_topic_sub)
+        client.subscribe(self.BIRTH_TOPIC)
         return client
 
-    def mqtt_callback(self, topic=None, msg=None):
-        print("Received MQTT message:", topic, msg)
+    def mqtt_callback(self, client, userdata, message, *args, **kwargs):
+        topic = message.topic
+        payload = message.payload.decode()
+        print(f"Received MQTT message: {client=}, {userdata=}, {message=}, {topic=}, {payload=}")
+        if topic == self.BIRTH_TOPIC and payload == "online":
+            self.discovery_sent = False
 
     def publish_msg(self, topic: str, message: str):
         self.mqtt_client.publish(topic, message)
         print(f"Published {topic}:", message)
 
     def publish_payload(self, topic: str, payload: dict):
+        self.send_auto_discovery(topic, payload)
         self.publish_msg(topic, json.dumps(payload))
+
+    def send_auto_discovery(self, topic: str, payload: dict):
+        if self.discovery_sent:
+            return
+
+        for key in payload:
+            unique_id = f"{self.topic_normalize(topic)}_{self.topic_normalize(key)}"
+            discovery_topic = f"homeassistant/sensor/{unique_id}/config"
+            unit = ""
+            if key.startswith("temp_"):
+                unit = "°C"
+            elif key.startswith("mem"):
+                unit = "%"
+
+            discovery_payload = {
+                "name": f"{key} {get_hostname()}",
+                "state_topic": topic,
+                "unit_of_measurement": unit,
+                "value_template": "{{ value_json.%s }}" % key,
+                "json_attributes_topic": topic,
+                "unique_id": unique_id,
+                "device": {
+                    "identifiers": [get_hostname()],
+                    "name": get_hostname(),
+                    "manufacturer": "PH4",
+                    "model": "Metrics Collector",
+                },
+            }
+            self.publish_msg(discovery_topic, json.dumps(discovery_payload))
+            print(f"Published auto-discovery to {discovery_topic}: {discovery_payload}")
+
+        self.discovery_sent = True
 
     def publish(self):
         self.publish_payload(
@@ -124,7 +169,16 @@ class MetricsCollector:
             self.compute_metrics()
             self.publish()
             print(f"Published metrics to {self.mqtt_broker}:{self.mqtt_port}: {self.metrics}")
-            time.sleep(60)
+            tstart = time.time()
+            while time.time() - tstart < 60:
+                try:
+                    self.mqtt_client.loop(timeout=1.0)  # Process network events
+                except Exception as e:
+                    print(f"Error in MQTT loop {e}")
+                    break
+
+            # self.mqtt_client.loop(timeout=60.0)  # Process network events
+            # time.sleep(60)
 
 
 if __name__ == "__main__":
